9f7ce500c96f59021c2ddb39c76ea18518c6a4ff
Agnee-he <15958021691@163.com>
2017-10-25 16:22:08 +0800

    11

--- mall-master/src/main.js
+++ mall-master/src/main.js
@@ -51,11 +51,11 @@ new Vue({
   methods:{
     checkLogin(){
       axios.get("users/checkLogin").then(res=> {
-        var res = res.data;
-        if (res.status == "0") {
+        let res = res.data;
+        if (res.status === "0") {
           this.$store.commit("updateUserInfo", res.result);
         }else{
-          if(this.$route.path!="/goods"){
+          if(this.$route.path!=="/goods"){
             this.$router.push("/goods");
           }
         }
@@ -63,8 +63,8 @@ new Vue({
     },
     getCartCount(){
       axios.get("users/getCartCount").then(res=>{
-        var res = res.data;
-        if(res.status=="0"){
+        let res = res.data;
+        if(res.status==="0"){
           this.$store.commit("updateCartCount",res.result);
         }
       });
